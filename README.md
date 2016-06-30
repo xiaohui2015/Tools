940da9d3a1cc35812263820ae99ed635148669ed
xiaohui2015 <2384618767@qq.com>
2016-06-30 17:18:33 +0800

    README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Tools
+# Tools  远程备份Xcode中的代码片段至Github
+
+1.将自定义的CodeSnippets文件夹拷贝至本地仓库
+2.将自定义的setup_snippets.sh文件拷贝到本地仓库
+3.终端输入cd 本地仓库
+4.终端输入./setup_snippets.sh
+操作成功后，便可以同步本地的Xcode中的代码片段了，从此本台设备无需二次操作！
+
---
+++ setup_snippets.sh
@@ -0,0 +1,6 @@
+#将Xcode下的该目录链接到本地Github仓库下的该目录（ln -s 目录A 目录B  意为：将B指向A，B是链接目录）
+
+ln -s /Users/fengxiaohui/Pictures/My\ Github\ Repository\ /Tools/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets
+
+echo “success”
+
